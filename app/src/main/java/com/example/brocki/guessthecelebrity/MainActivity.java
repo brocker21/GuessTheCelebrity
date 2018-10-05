3357fefc6a3f62042f3915eefadef507bd982da7
Jarek Brocki <jarek.brocki@gmail.com>
2018-10-05 11:34:12 +0200

    Done!

--- app/src/main/java/com/example/brocki/guessthecelebrity/MainActivity.java
+++ app/src/main/java/com/example/brocki/guessthecelebrity/MainActivity.java
@@ -16,15 +16,24 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
-    Button button1,button2,button3,button4;
+    Button button1, button2, button3, button4;
     ImageView picture;
     String result;
     ArrayList<String> photoURLresult = new ArrayList<>();
     ArrayList<String> celebrityNames = new ArrayList<>();
+    int maxArrayLength=0;
+    int pickedCelebrity = 0;
+    String pickedCelebrityName="";
+    Random rand;
+    ArrayList<String> possibleAnswers = new ArrayList<>();
+    int score,numberOfQuestions = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,43 +45,50 @@ public class MainActivity extends AppCompatActivity {
         button3 = findViewById(R.id.buttonAnswer3);
         button4 = findViewById(R.id.buttonAnswer4);
         picture = findViewById(R.id.imageViewCelebrity);
+        rand = new Random();
+        populateAtStart();
     }
 
-        public class imageDownloaderTask extends AsyncTask<String, Void, Bitmap> {
+    public class imageDownloaderTask extends AsyncTask<String, Void, Bitmap> {
 
-            @Override
-            protected Bitmap doInBackground(String... urls) {
-                try {
-                    URL url = new URL(urls[0]);
-                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-                    connection.connect();
-                    InputStream in = connection.getInputStream();
-                    Bitmap myBitmap = BitmapFactory.decodeStream(in);
-                    return myBitmap;
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    return null;
-                }
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+            try {
+                URL url = new URL(urls[0]);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+                InputStream in = connection.getInputStream();
+                Bitmap myBitmap = BitmapFactory.decodeStream(in);
+                button1.setText(possibleAnswers.get(0));
+                button2.setText(possibleAnswers.get(1));
+                button3.setText(possibleAnswers.get(2));
+                button4.setText(possibleAnswers.get(3));
+                return myBitmap;
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
             }
-
         }
 
-    public void getHTTPData(){
+    }
+
+    public void getHTTPData() {
         httpDownloadTask httpDownloadTask = new httpDownloadTask();
         try {
             result = httpDownloadTask.execute("http://www.posh24.se/kandisar").get();
 
             Pattern p = Pattern.compile("src=\"http://(.*?)\"");
             Matcher m = p.matcher(result);
-            while (m.find()){
+            while (m.find()) {
                 photoURLresult.add(m.group(1));
-           }
-           Pattern p2 = Pattern.compile("alt=(.*?)/>");
+            }
+            Pattern p2 = Pattern.compile("alt=\"(.*?)\"/>");
             Matcher m2 = p2.matcher(result);
-            while (m2.find()){
+            while (m2.find()) {
                 celebrityNames.add(m2.group(1));
             }
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -81,19 +97,19 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected String doInBackground(String... urls) {
-            String result= "";
+            String result = "";
             URL url;
             HttpURLConnection urlConnection = null;
 
             try {
                 url = new URL(urls[0]);
-                urlConnection = (HttpURLConnection)url.openConnection();
+                urlConnection = (HttpURLConnection) url.openConnection();
                 InputStream in = urlConnection.getInputStream();
                 InputStreamReader reader = new InputStreamReader(in);
                 int data = reader.read();
                 while (data != -1) {
                     char current = (char) data;
-                    result+=current;
+                    result += current;
                     data = reader.read();
                 }
                 return result;
@@ -107,17 +123,46 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    /* public void downloadImage (View view) {
+    public void downloadImage(String url) {
+
         imageDownloaderTask task = new imageDownloaderTask();
         Bitmap myImage;
         try {
-            myImage = task.execute("http://cdn.posh24.se/images/:profile/03f352f71ffab135cd81821eb190d4832").get();
+            myImage = task.execute(url).get();
             picture.setImageBitmap(myImage);
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
-    } */
-    public void downloadImage (View view) {
-       getHTTPData();
+    }
+
+    public void checkAnswer(View view) {
+        if (possibleAnswers.get(Integer.parseInt(""+view.getTag())).equals(pickedCelebrityName)){
+            score+=1;
+            numberOfQuestions+=1;
+            Toast.makeText(this, "Correct", Toast.LENGTH_SHORT).show();
+            generateQuestion();
+        } else {
+            numberOfQuestions+=1;
+            Toast.makeText(this, "Wrong!", Toast.LENGTH_SHORT).show();
+            generateQuestion();
+        }
+    }
+
+    public void generateQuestion() {
+        possibleAnswers.clear();
+        pickedCelebrity = rand.nextInt(maxArrayLength);
+        pickedCelebrityName=celebrityNames.get(pickedCelebrity);
+        possibleAnswers.add(pickedCelebrityName);
+        possibleAnswers.add(celebrityNames.get(rand.nextInt(maxArrayLength)));
+        possibleAnswers.add(celebrityNames.get(rand.nextInt(maxArrayLength)));
+        possibleAnswers.add(celebrityNames.get(rand.nextInt(maxArrayLength)));
+        Collections.shuffle(possibleAnswers);
+        downloadImage("http://"+photoURLresult.get(pickedCelebrity));
        }
+
+    public void populateAtStart(){
+        getHTTPData();
+        maxArrayLength = celebrityNames.size();
+        generateQuestion();
+    }
 }
